beba453e8a889f542b8554c14ed78e31a896beef
Nicola <nicola@nicola-desk.lan>
2017-10-11 14:47:58 +0300

    Добавил Delete Секрета

--- src/main/java/com/secret/controllers/SecretController.java
+++ src/main/java/com/secret/controllers/SecretController.java
@@ -1,22 +1,33 @@
 package com.secret.controllers;
 
+import com.secret.domain.Repository;
 import com.secret.domain.Secret;
+import com.secret.services.RepositoryService;
 import com.secret.services.SecretsService;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.MediaType;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.POST;
 
 /**
  * Created by nicola on 10.10.17.
@@ -30,6 +41,13 @@ public class SecretController {
     private Logger log = Logger.getLogger(String.valueOf(RepoController.class));
 
     private SecretsService secretsService;
+
+    private RepositoryService repositoryService;
+
+    @Autowired
+    public void setRepositoryService(RepositoryService repositoryService) {
+        this.repositoryService = repositoryService;
+    }
 
     @Autowired
     public void setSecretsService(SecretsService secretsService) {
@@ -64,23 +82,19 @@ public class SecretController {
 
 
         model.addAttribute("secrets", secretsService.listAllSecrets());
+        model.addAttribute("repos", name);
         return "secrets";
     }
 
-    @RequestMapping(value = "repos/{owner}/{name:.+}/{secretName}", method = RequestMethod.DELETE)
+    @RequestMapping("repos/{owner}/{name:.+}/{secretName}")
     public String deleteSecret(@PathVariable String owner,@PathVariable String name,@PathVariable String secretName) {
         String url = "https://drone.ts-t.ru/api/repos/".concat(owner).concat("/").concat(name).concat("/secrets/".concat(secretName));
-        try {
-            Client client = Client.create();
-            WebResource webResource = client.resource(url);
-            ClientResponse response = webResource.type(MediaType.APPLICATION_JSON)
-                    .header("Authorization", token)
-                    .delete(ClientResponse.class);
-            log.info("response2: " + response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "redirect:/repos/";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token);
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        restTemplate.exchange(url, DELETE, entity, String.class);
+        return "redirect:/repos/".concat(owner).concat("/").concat(name);
     }
 
 //    @RequestMapping("global/edit/{id}")
